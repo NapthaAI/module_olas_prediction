ba082e570340556733db10b22afade2987b42ff6
Richard Blythman <richardblythman@gmail.com>
2024-09-25 23:47:23 +0100

    update predication agent

--- README.md
+++ README.md
@@ -1,19 +1,5 @@
-# Prediction Request
+# Prediction Agent
 
-## Install
+This is a simple agent that makes a prediction.
 
-```console
-git clone https://huggingface.co/NapthaAI/olas_prediction
-cd olas_prediction
-
-# set up env
-poetry install
-poetry shell
-```
-
-## Test 
-
-```console
-chmod +x tests/test_olas_prediction.py
-poetry run tests/test_olas_prediction.py
-```
+Follow the instructions to run it on a Naptha Nodefrom the [Naptha SDK](https://github.com/NapthaAI/naptha-sdk).
--- prediction_agent/run.py
+++ prediction_agent/run.py
@@ -1,31 +1,8 @@
-# -*- coding: utf-8 -*-
-# ------------------------------------------------------------------------------
-#
-#   Copyright 2023-2024 Valory AG
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-#
-# ------------------------------------------------------------------------------
-
-"""This module implements a Mech tool for binary predictions."""
-
 from litellm import completion
-from olas_prediction.schemas import InputSchema
-from olas_prediction.utils import get_logger
-import os
+from prediction_agent.schemas import InputSchema
+from prediction_agent.utils import get_logger
 import re
 from typing import Any, Dict, Optional, Tuple
-import yaml 
 
 logger = get_logger(__name__)
 
@@ -38,7 +15,7 @@ def extract_json_string(text):
 def run(inputs: InputSchema, cfg) -> Tuple[str, Optional[str], Optional[Dict[str, Any]], Any]:
     """Run the task"""
 
-    prediction_prompt = cfg["inputs"]["prediction_prompt"].format(question=inputs.prompt)
+    prediction_prompt = cfg["inputs"]["prediction_prompt"].format(prompt=inputs.prompt)
     messages = [
         {"role": "system", "content": cfg["inputs"]["system_message"]},
         {"role": "user", "content": prediction_prompt},
